6a0b258c354ef03b928ea5c273161c9147494ea0
Jecht89 <107807465+Jecht89@users.noreply.github.com>
2022-09-27 22:04:11 +0200

    Update pagina_p1.php

--- pagina_p1.php
+++ pagina_p1.php
@@ -1,39 +1,59 @@
-
-
 <!DOCTYPE HTML>
 <html>
-    
-    <h1>Esercizio con metodo get</h1>
-    <form action="<?php $_SERVER['SCRIPT_NAME'] ?>" method='get'>
+
+<h1>Esercizio con metodo get</h1>
+<form action="<?php $_SERVER['SCRIPT_NAME'] ?>" method='GET'>
     <p>Inserire nome <input type='text' name='nome' placeholder='Nome'></p>
     <p>Inserire cognome <input type='text' name='cognome' placeholder='Cogome'></p>
-    <p>Inserire data di Nascita <input type='date' name='nascita' placeholder='dd-mm-yyyy'></p>  
+    <p>Inserire data di Nascita <input type='date' name='nascita' placeholder='dd-mm-yyyy'></p>
     <p>Inserire un numero <input type='number' name='numero_r' placeholder='Inserisci un Numero'></p>
     <input type='submit' name='submit_invia' value='Invia'>
 
-    </form>
+</form>
+
 </html>
 
 <?php
 
-if (isset($_GET['submit_invia'])) {
-    $data_nascita = date('Y', strtotime($_GET['nascita']));
-    $data_oggi = date('Y'); // current date-time
-    $anni = ($data_oggi-$data_nascita);
-    if ($_GET['numero_r'] <= 3){        
-        echo "Il nome passato è: {$_GET['nome']}, ";
-        echo "il cognome passato è: {$_GET['cognome']}, ";
-        echo "La data di nascita è: {$_GET['nascita']} e hai: $anni Anni.<br>";
-        }
+if ($_GET){
 
-    if ($_GET['numero_r'] > 3){
-        for ($i=0; $i < $_GET['numero_r'] ; $i++) { 
-            echo "Il nome passato è: {$_GET['nome']}, ";
-            echo "il cognome passato è: {$_GET['cognome']}, ";
-            echo "La data di nascita è: {$_GET['nascita']} e hai: $anni anni.<br>";
-        }
+   if($_GET['numero_r']<=3){
+      echo "Il nome passato è: ". $_GET['nome'] .", ";
+      echo "il cognome passato è: ". $_GET['cognome'] .", ";
+      controllo_anni($_GET['nascita']);
 
-    }
+   }else{
+      for ($i = 0; $i < $_GET['numero_r']; $i++) {
+         echo "Il nome passato è: ". $_GET['nome'] .", ";
+         echo "il cognome passato è: ". $_GET['cognome'] .", ";
+         controllo_anni($_GET['nascita']);
+      }
+   }   
+}
+
+
+function controllo_anni($data_nascita){
+   // data di nascita 
+   $nascita = date('d-m-Y', strtotime($data_nascita));
+   $giorno_nascita = date('d', strtotime($data_nascita));
+   $mese_nascita = date('m', strtotime($data_nascita));
+   $anno_nascita = date('Y', strtotime($data_nascita));   
+   // data corrente
+   $giorno_oggi = date('d');
+   $mese_oggi = date('m');
+   $anno_oggi = date('Y');   
+   // calcolo età
+   $anni = $anno_oggi-$anno_nascita;
+   // controllo anni
+   if($giorno_oggi == $giorno_nascita && $mese_oggi == $mese_nascita){
+      echo 'la data di nascita è: '. $nascita . " e oggi compi ". $anni ." anni. AUGIRI!<br>";
+   }
+   if($giorno_oggi > $giorno_nascita && $mese_oggi == $mese_nascita || $mese_oggi > $mese_nascita){
+      echo "la data di nascita è: ". $nascita . " e hai ". $anni ." anni.<br>";
+   }
+   if($giorno_oggi < $giorno_nascita && $mese_oggi == $mese_nascita || $mese_oggi < $mese_nascita){
+      echo "la data di nascita è: ". $nascita . " e hai ". ($anni-1) . " anni.<br>";
+   }
 }
 
 ?>
